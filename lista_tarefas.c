6fd7c8f6daa2a8271c13c7f11ef0d9f656f50844
Luiz Gustavo Zanoni <luizzanoni47@gmail.com>
2024-06-24 19:35:51 -0300

    ordenação da lista

--- lista_tarefas.c
+++ lista_tarefas.c
@@ -60,18 +60,63 @@ void excluirTarefa(ListaDeTarefas *lista, int id) {
     printf("Tarefa ID %d não encontrada.\n", id);
 }
 
-// Função para imprimir todas as tarefas da lista
+int compararPorTempoLimite(const void *t1, const void *t2) {
+    Tarefa *tarefa1 = *(Tarefa **)t1;
+    Tarefa *tarefa2 = *(Tarefa **)t2;
+    
+    if (tarefa1->tempo_limite < tarefa2->tempo_limite) return -1;
+    if (tarefa1->tempo_limite > tarefa2->tempo_limite) return 1;
+    return 0;
+}
+
 void imprimirTodasTarefas(ListaDeTarefas *lista) {
-    printf("Todas as Tarefas:\n");
+    printf("Todas as Tarefas (ordenadas por tempo limite):\n");
     printf("ID\tDescrição\t\tTempo Limite\tConcluída\n");
     printf("-------------------------------------------------\n");
 
+    // Contar o número de tarefas na lista
+    int num_tarefas = 0;
     Tarefa *atual = lista->cabeca;
     while (atual != NULL) {
-        imprimirTarefa(atual);
+        num_tarefas++;
         atual = atual->prox;
     }
+
+    // Criar um array de ponteiros para Tarefa
+    Tarefa **tarefas_array = (Tarefa **)malloc(num_tarefas * sizeof(Tarefa *));
+    
+    // Preencher o array com as tarefas da lista
+    atual = lista->cabeca;
+    int i = 0;
+    while (atual != NULL) {
+        tarefas_array[i++] = atual;
+        atual = atual->prox;
+    }
+
+    // Ordenar o array de tarefas pelo tempo limite
+    qsort(tarefas_array, num_tarefas, sizeof(Tarefa *), compararPorTempoLimite);
+
+    // Imprimir as tarefas ordenadas
+    for (int j = 0; j < num_tarefas; j++) {
+        imprimirTarefa(tarefas_array[j]);
+    }
+
+    // Liberar a memória do array de ponteiros
+    free(tarefas_array);
 }
+
+// Função para imprimir todas as tarefas da lista
+// void imprimirTodasTarefas(ListaDeTarefas *lista) {
+//     printf("Todas as Tarefas:\n");
+//     printf("ID\tDescrição\t\tTempo Limite\tConcluída\n");
+//     printf("-------------------------------------------------\n");
+
+//     Tarefa *atual = lista->cabeca;
+//     while (atual != NULL) {
+//         imprimirTarefa(atual);
+//         atual = atual->prox;
+//     }
+// }
 
 // Função para imprimir somente as tarefas ativas (não concluídas)
 void imprimirTarefasAtivas(ListaDeTarefas *lista) {
